fcc1bc1abd8e9173bd884ac865371e22e6c432ae
samanjhutty <jhuttysaman@gmail.com>
2023-04-05 02:41:51 +0530

    list questions complete

--- ListExample.dart
+++ ListExample.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  compareLists();
+  firstLast();
 }
 
 printLessNumbers() {
@@ -9,7 +9,7 @@ printLessNumbers() {
 
   for (int i in list) {
     if (i < 5) {
-      stdout.write("${list[i]} ");
+      write("${list[i]} ");
     }
   }
 }
@@ -26,11 +26,47 @@ compareLists() {
       }
     }
   }
-  stdout.writeln(c);
+  writeln(c);
   for (int k = 0; k < c.length - 1; k++) {
     if (c[k] == c[k + 1]) {
       c.remove(c[k + 1]);
     }
   }
-  stdout.writeln(c);
+  writeln(c);
+}
+
+evenElements() {
+  List<int> list = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+  List dummy = [];
+
+  for (int i in list) {
+    if (i % 2 == 0) {
+      dummy.add(i);
+    }
+  }
+  write("New list is: $dummy");
+}
+
+firstLast() {
+  writeln("Enter no of elements to be entered: ");
+  int length = int.parse(stdin.readLineSync()!);
+  write("Enter elements of list: ");
+
+  List list = [];
+  for (int i = 0; i < length; i++) {
+    list.add(int.parse(stdin.readLineSync()!));
+  }
+  write("Entered list: $list");
+  List<int> list2 = [];
+  list2.add(list.first);
+  list2.add(list.last);
+  writeln("new list: $list2");
+}
+
+write(var write) {
+  stdout.write(write);
+}
+
+writeln(var write) {
+  stdout.writeln(write);
 }
